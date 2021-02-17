444bb195d48fa2f5f9ed1f2e325736bb89a6c27f
LittleRedRidingU <LittleRedRidingU@users.noreply.github.com>
2021-02-17 17:16:30 -0500

    enlarge sidebar on hover

--- src/main/index.js
+++ src/main/index.js
@@ -20,9 +20,9 @@ function createWindow () {
    * Initial window options
    */
   mainWindow = new BrowserWindow({
-    height: 563,
+    height: 1024,
     useContentSize: true,
-    width: 1000
+    width: 1440
   })
 
   mainWindow.loadURL(winURL)
