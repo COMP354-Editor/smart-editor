dfd8b080575cc94b52fa52f78e6aa8856ebc62dd
Fu <sevenliu18@gmail.com>
2021-04-16 22:22:44 -0400

    io-saveAs

--- src/renderer/service/IOService.ts
+++ src/renderer/service/IOService.ts
@@ -1,47 +1,79 @@
 const  fs  = require('fs')
 const { dialog } = require('electron').remote
 
+
 import {SaveDialogReturnValue, OpenDialogReturnValue} from 'electron'
 import textCharManager from "../model/TextCharManager";
 
 class IOService {
 
+    selectedFilePath: string;
+
     constructor() {
+        this.selectedFilePath = "";
     }
+
     open() {
-        dialog.showOpenDialog({ properties: ['openFile', 'openDirectory'] })
+        return new Promise ((resolve) => {
+            
+            dialog.showOpenDialog({ properties: ['openFile', 'openDirectory'] })
             .then((fileNames: OpenDialogReturnValue) => {
                 if (fileNames === undefined) {
-                    console.log("No File Selected");
+                    alert("No File Selected");
                 } else {
-                    //Test For local file path, it works
+                    //store File Path
+                    this.selectedFilePath = fileNames.filePaths[0];             
+                    //Node JS call reading file method
                     fs.readFile(fileNames.filePaths[0], 'utf-8', function (err: { message: string; }, data: string) {
                         if (err) {
-                            alert("Error Occurred Reading The File : " + err.message);
+                            console.error("Error Occurred Reading The File : " + err.message);
                             return;
                         }
                         textCharManager.createTextChar(0, data)
+                        resolve(data);
                     });
                 }
             })
+        })
     }
-
+    //save as NEW file
     saveAs() {
         const content = textCharManager.getTextValue();
         //Call Dialog Window
         dialog.showSaveDialog({ properties: ['showHiddenFiles'] })
             .then((fileName:SaveDialogReturnValue) => {
                 if (fileName === undefined) {
-                    console.log("You Can Not Save The File.");
+                    alert("You Can Not Save The File.");
                     return;
                 }
+                //Node JS call reading file method
                 fs.writeFile(fileName.filePath, content, function (err: { message: string; }) {
                     if (err) {
                         console.error("Error occurred creating the file : " + err.message)
                     }
-                    console.log("The File Has Been Saved.");
+                    alert("The File Has Been Saved.");
                 });
             });
+    }
+    //open exist file and edited, then save to same file
+    save(){
+        //get content
+        const content = textCharManager.getTextValue();
+
+        if(this.selectedFilePath.length > 0){
+            //Node JS call File Method
+            fs.writeFile(this.selectedFilePath, content, function (err: { message: string;}) {
+
+            if(err){
+                console.error("Error ocurred updating the file : "+ err.message);
+                console.log(err);
+                return;
+            }
+            alert("The File Has Been Saved.");
+            }); 
+        }else{
+            alert("Please Select A File.");
+        }      
     }
 }
 
